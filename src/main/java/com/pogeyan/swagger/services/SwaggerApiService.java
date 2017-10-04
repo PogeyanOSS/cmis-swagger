154d375a5f4d2dac7e01ce5d653100821375f1a5
Suhaib Fahad <fahad.suhaib@pogeyan.com>
2017-10-04 22:20:44 +0530

    FIX: updating license headers

--- src/main/java/com/pogeyan/swagger/services/SwaggerApiService.java
+++ src/main/java/com/pogeyan/swagger/services/SwaggerApiService.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2017 Pogeyan Technologies
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.pogeyan.swagger.services;
 
 import java.io.IOException;
@@ -65,7 +80,7 @@ public class SwaggerApiService {
 	 *            the property typeId of an object-type specified in the
 	 *            repository.
 	 * @param parentId
-	 *            the property parentId is used to get the object-type’s
+	 *            the property parentId is used to get the object-typeï¿½s
 	 *            immediate parent type.
 	 * @param input
 	 *            the property input is used to get all request parameters.
@@ -244,7 +259,7 @@ public class SwaggerApiService {
 	 *            the property typeId of an object-type specified in the
 	 *            repository.
 	 * @param parentId
-	 *            the property parentId is used to get the object-type’s
+	 *            the property parentId is used to get the object-typeï¿½s
 	 *            immediate parent type.
 	 * @param input
 	 *            the property input is used to get all request parameters.
