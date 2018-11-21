061996593d1b6061834982b236929f3bb600823c
RohiniKambam <rohinireddy01.k@gmail.com>
2018-11-21 17:42:14 +0530

    created newmanfile

---
+++ copper-swagger/src/test/newman.js
@@ -0,0 +1,15 @@
+var newman = require("newman");
+newman.run(
+{
+collection: require("./SwaggerApiRequest.postman_collection.json"),
+reporters: "cli",
+globals: require("./NewWorkspace.postman_globals.json"),
+iterationCount: 1
+},
+function(err) {
+if (err) {
+throw err;
+}
+console.log("collection run completed!");
+}
+);
