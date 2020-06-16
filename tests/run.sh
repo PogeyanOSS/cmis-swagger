39d4ef94e3efd2eb9699f7475549769d4f9a06ee
Praveen Kumar Konathala <praveen.k@pogeyan.com>
2020-06-16 17:48:28 +0530

    setting the env in globals json

--- tests/run.sh
+++ tests/run.sh
@@ -3,6 +3,9 @@ export CMIS_USERNAME=$2
 export CMIS_PASSWORD=$3
 export CMIS_REPO_ID=$4
 export HOSTURL=$5
+export REQUEST_URL=$6
+export EMAIL=$7
+export PASWORD=$8
 
 echo "clearing the db"
 
--- tests/swaggerTests.js
+++ tests/swaggerTests.js
@@ -1,9 +1,11 @@
 var newman = require("newman");
+const global = require("./globals.js").globals;
+const globalJson = JSON.parse(JSON.stringify(global));
 newman.run(
     {
         collection: require("./SwaggerApiRequest.postman_collection.json"),
         reporters: ["cli", "html"],
-        globals: require("./globals.json"),
+        globals: globalJson,
         iterationCount: 1,
         reporter: {
             html: {
